a61eef9e670552210012d91d56f771c55a0cc4c6
Wallacewss2033 <wallace-2033@hotmail.com>
2024-03-01 19:35:07 -0300

    feat: criar a tabela pivot entre order e product e os relacionamentos entre elas

---
+++ app/Models/OrderProduct.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class OrderProduct extends Pivot
+{
+    protected $fillable = [
+        'order_id',
+        'product_id'
+    ];
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
