bbc983b79a11de3c77c6cbd4865e6ba0cf3147a9
Wallacewss2033 <wallace-2033@hotmail.com>
2024-03-01 16:30:14 -0300

    test: criar teste para o modelo de pedido

---
+++ tests/Feature/app/Models/OrderTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature\app\Models;
+
+use App\Models\Order;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class OrderTest extends TestCase
+{
+    public function test_model_of_order(): void
+    {
+        $order = Order::factory()->create([
+            'amount' => 21.98,
+            'sales_id' => 2,
+            'status' => 1,
+        ]);
+
+        $this->assertDatabaseHas('orders', [
+            'amount' => $order->amount,
+            'sales_id' => $order->sales_id,
+            'status' => $order->status,
+        ]);
+    }
+}
