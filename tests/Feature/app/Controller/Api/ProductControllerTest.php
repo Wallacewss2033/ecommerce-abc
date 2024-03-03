544b5b87316ad39d11b991dcf8bacfc184430962
Wallacewss2033 <wallace-2033@hotmail.com>
2024-03-03 03:32:56 -0300

    test: criar teste do endpoint de  listagem dos produtos

---
+++ tests/Feature/app/Controller/Api/ProductControllerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature\app\Controller\Api;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class ProductControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->get('/api/products');
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'current_page',
+                'data',
+                'first_page_url',
+                'from',
+                'last_page',
+                'last_page_url',
+                'links',
+                'next_page_url',
+                'path',
+                'per_page',
+                'prev_page_url',
+                'to',
+                'total',
+            ])->assertJsonCount(10, 'data');
+    }
+}
